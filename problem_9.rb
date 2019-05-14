6b41860f2acb92458057a905c3f7f09f860619ec
Sunil Kumar <sunilsmachine@Sunils-MacBook-Air.local>
2019-05-14 15:04:22 +0530

    Problem 9

---
+++ problem_9.rb
@@ -0,0 +1,19 @@
+def pythagorus_triplet
+  max, product = 500, 0
+
+  for a in (1..max)
+    for b in (a..max)
+      for c in (b..max)
+        if a**2 + b**2 == c**2 and a+b+c == 1000
+          puts a, b, c
+          product =  a * b * c
+          break
+        end
+      end
+    end
+  end
+
+  product
+end
+
+puts pythagorus_triplet
